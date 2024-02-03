e1b333babbe96812b3ece4b4b79e1bcc1354d7f9
hiro1425 <goegoe1425@gmail.com>
2024-02-03 18:13:49 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,23 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
+  validates :items_name, presence: true
+  validates :explain, presence: true
+  validates :category_id, presence: true
+  validates :situation_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :area_id, presence: true
+  validates :shipping_day_id, presence: true
+  with_options presence: true, format: { with: /\A[0-9]+\z/} do
+    validates :price,  numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+    presence: { message: "can't be blank" }
+  end
+  
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :situation_id
     validates :delivery_charge_id
-    validates :area
-    validates :shipping_day
+    validates :area_id
+    validates :shipping_day_id
   end
 
   belongs_to :user
