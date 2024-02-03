ad5e2a09bef491e99e5a63c13c0e2d4fa3a201c3
hiro1425 <goegoe1425@gmail.com>
2024-02-03 17:25:12 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :situation_id
+    validates :delivery_charge_id
+    validates :area
+    validates :shipping_day
+  end
+
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :delivery_charge
+  belongs_to :area
+  belongs_to :shipping_day
 end
