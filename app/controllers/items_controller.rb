2a806ce5fe45a5575750b444970f180856c2d47e
hiro1425 <goegoe1425@gmail.com>
2024-02-05 14:32:25 +0900

    商品出品ページの表示と商品出品の際のテーブルへの保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,22 @@
 class ItemsController < ApplicationController
-  def index
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
-  def message_params
+  def items_params
     params.require(:item).permit(:image, :items_name, :explain, :category_id, :situation_id, :delivery_charge_id, :area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 end
