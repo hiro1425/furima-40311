5848cd74fa329e660e40636ab0694d644c1d7277
hiro1425 <goegoe1425@gmail.com>
2024-02-06 15:51:29 +0900

    販売手数料や販売利益の表示

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,12 @@
-window.addEventListener('turbo:load', () => {
-  console.log("OK");
-});
+function price() {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profit = document.getElementById("profit");
+    
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    profit.innerHTML = Math.floor(inputValue - inputValue * 0.1);
+})}
+
+window.addEventListener('turbo:load', price)
