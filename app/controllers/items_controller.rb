6490daec30fe3d4ea62143b622761ba40956a44a
hiro1425 <goegoe1425@gmail.com>
2024-02-09 16:44:33 +0900

    商品削除機能：修正版

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :redirect_if_not_author, only: [:edit]
+  before_action :redirect_if_not_author, only: [:edit, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
