c1b69aa9ebd1de45e348a976a737993ae5cda7f8
hiro1425 <goegoe1425@gmail.com>
2024-02-12 14:46:27 +0900

    購入フォームの実装とデータベースへ購入者情報を保存

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,8 +1,22 @@
 class BuysController < ApplicationController
   def index
     @buy_address = BuyAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @buy_address = BuyAddress.new(buy_params)
+    if @buy_address.valid?
+      @buy_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def buy_params
+    params.require(:buy_address).permit(:postal_code, :area_id, :city, :address, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
   end
 end
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,6 +1,6 @@
 class BuyAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :area, :city, :address, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :area_id, :city, :address, :building, :phone_number
 
   with_options presence: true do
     validates :user_id
@@ -10,10 +10,10 @@ class BuyAddress
     validates :address
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "は10桁または11桁の数字で入力してください"}
   end
-  validates :area, numericality: {other_than: 1, message: "can't be blank"}
+  validates :area_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
     buy = Buy.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, area: area, city: city, address: address, building: building, phone_number: phone_number, buy_id: buy_id)
+    Address.create(postal_code: postal_code, area_id: area_id, city: city, address: address, building: building, phone_number: phone_number, buy_id: buy.id)
   end
 end
